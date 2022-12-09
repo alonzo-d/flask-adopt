c85a9c51c5623064e172be173697e8427eb04944
David A. Salazar <davidalonzso@gmail.com>
2022-12-09 12:01:23 -0800

    add class Pet

--- models.py
+++ models.py
@@ -14,3 +14,46 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+
+    __tablename__ = 'pets'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.Text,
+        nullable = False,
+    )
+
+    species = db.Column(
+        db.Text,
+        nullable = False,
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable = False,
+        default = ''
+    )
+
+    age = db.Column(
+        db.Text,
+        nullable = False,
+        #db.CheckConstraint("age In ['baby', 'young', 'adult', 'senior']")
+    )
+
+    notes = db.Column(
+        db.Text,   
+    )
+
+    available = db.Column(
+        db.Text,
+        nullable = False,
+        default = 'Available'
+    )
