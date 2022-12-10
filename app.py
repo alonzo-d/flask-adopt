3777e987e942e905e2622feb23e303dec9cb2b4d
Nico Martinucci <domenico.martinucci@gmail.com>
2022-12-09 16:56:13 -0800

    add function to get pet finder oauth token

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 """Flask app for adopt app."""
 
+ACCESS_TOKEN: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJXRFYxV1g4Q3NaSjUyWmJ3dDVlZDlTNEJ1Q3g3Qkh0c0w5OW54WU5BYjRjNzN6ellEMyIsImp0aSI6IjZhY2M2Yjk3Njc2ZWQ4ZDYwZTk1N2MwZDBhNDJjYmY3NWM5NTFiY2YxYTJmZDA5NTE2Njc3OTgwZDQ5ODJiNzE4ZTE5NzNlZWM2M2QzY2M0IiwiaWF0IjoxNjcwNjMyNDAyLCJuYmYiOjE2NzA2MzI0MDIsImV4cCI6MTY3MDYzNjAwMiwic3ViIjoiIiwic2NvcGVzIjpbXX0.u3Vjq5BqTF73gmd07_8NxYTYZPgsJz4JWYsvHBo2JcM468aUF_nqIqR70RSj9Dl-d-oru_Qm0sMBcEjQtYIAuyLlyPYnw7opuUI7jujsnhm_kAJUiGBkQe59h0mTSAndwzbNY6vfjehUZYvM9m2j7pDD-c3GMks2JLtCQLyJSUR62Mk0_tPwUXjy3ffDBCGasbA5xaeujO7DeVQBgFGoADc14YmymQdv8mr0dQSeeY3afh17gjqJkSXs-LZJ8WHxt9FhXcro_oH8QX7NVHqSHywZzAK817e9Smt1vnr11pbV2CuVu__qPtoBdiz9ZvOLZppvq_hnP8gO8SNj8SVxwA"
+
+
 from flask import Flask, flash, request, redirect, render_template
-
 from flask_debugtoolbar import DebugToolbarExtension
-
 from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
+from dotenv import load_dotenv
+import os
+import requests
 
 app = Flask(__name__)
 
@@ -24,6 +28,24 @@ db.create_all()
 #
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+API_ACCESS_KEY = os.environ['PETFINDER_API_KEY']
+API_SECRET_KEY = os.environ['PETFINDER_SECRET_KEY']
+
+def get_pet_finder_oauth_token():
+    resp = requests.post(
+        url="https://api.petfinder.com/v2/oauth2/token",
+        data={
+            "grant_type": "client_credentials",
+            "client_id": API_ACCESS_KEY,
+            "client_secret": API_SECRET_KEY
+        }
+    ) # this is JSON!
+
+    return resp.json()["access_token"] # .json() --> python dictionary
+
+API_OAUTH_TOKEN = get_pet_finder_oauth_token()
+
+breakpoint()
 
 @app.get('/')
 def show_home_page():
